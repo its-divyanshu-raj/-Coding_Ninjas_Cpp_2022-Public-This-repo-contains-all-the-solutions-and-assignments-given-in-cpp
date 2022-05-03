9b7d48e570cc450e40c384334b14a4df55aca869
its-divyanshu-raj <98219015+its-divyanshu-raj@users.noreply.github.com>
2022-05-03 10:36:59 +0530

    Create THE no. is positive.cpp

---
+++ THE no. is positive.cpp
@@ -0,0 +1,26 @@
+/* Check number
+You are given a single number. You need to print one of the following outputs according to the number's nature.
+Print 1, if the number is positive
+Print -1, if it's negative
+Print 0, if it's equal to 0 */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int a ;
+  
+cin>>a;
+if (a>0)
+   cout<<"1";
+if (a<0)
+   cout<<"-1";
+if (a==0)
+   cout<<"0";
+   }
+
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
