2e48d1e202d91ed8dc2e037de542d2a5bc4d6b25
Divyanshu raj <98219015+its-divyanshu-raj@users.noreply.github.com>
2022-05-12 22:53:55 +0530

    Create Adressoff.cpp

---
+++ Adressoff.cpp
@@ -0,0 +1,13 @@
+   /* To find the storage  or  address  of our input integer or variable 
+
+
+
+ ************************************************************************************************************************************************************ */ 
+#include <iostream>
+using namespace std;
+int main(){
+  int i ;
+  
+  cout<< &i << endl;
+
+}
