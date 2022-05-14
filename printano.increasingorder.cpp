90694cf40fc9eaefeaa4b1d44c5b6a05d0d399c8
Divyanshu raj <98219015+its-divyanshu-raj@users.noreply.github.com>
2022-05-15 00:16:50 +0530

    Create printano.increasingorder.cpp

---
+++ printano.increasingorder.cpp
@@ -0,0 +1,20 @@
+ // To print a no. in order of lower to upper
+
+#include <iostream>
+using namespace std;
+int main(){
+
+int n;
+
+cout <<"enter n" << endl;
+
+cin>> n;
+
+int i = 1;
+
+while (i<=n) {
+    cout << i <<  endl;
+    i = i + 1 ;
+
+}
+}
